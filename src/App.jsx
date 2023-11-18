49d4752c83b8a187595efcba612d6b17394e8fcb
Modesto Rafael Alejandro Arreola Lira <modestorarreola@MacBook-Pro-de-Modesto.local>
2023-11-18 16:24:36 -0600

    bugfix vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import NavBar from "./components/NavBar"
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Cart from "./components/Cart";
-import About from "./components/About";
 import ItemListContainer from "./components/Item/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetail/ItemDetailContainer";
 
